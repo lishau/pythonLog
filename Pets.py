3094d5c7f062d6bff5395d6b1478b56c031cb9f8
Lidia <lidiamarchese@Lidias-MacBook-Pro.local>
2026-06-01 21:40:22 -0400

    I just created new file Pets.py

---
+++ Pets.py
@@ -0,0 +1,1 @@
+Pets={'Dogs': [{'dog': 'Chia'}, {'dog': 'Pepe'}, {'dog': 'Dodo'}], 'Cats': [{'cat': 'Kiki'}, {'cat': 'Tigra'}, {'cat': 'Gris'}, {'cat': 'Clarito'}], 'Bunnies': [{'bunny': 'Luna'}, {'bunny': 'Rico'}, {'bunny': 'Coco'}]}
