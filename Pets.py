f75bf209b5d7daec5db4fa2ac6b48e02aa40c22a
Eliezer Rodriguez <elie@Eliezers-Mac-mini.local>
2026-06-02 19:45:35 -0400

    Added a comment to do a first Pull Request

--- Pets.py
+++ Pets.py
@@ -1,1 +1,4 @@
+
+# This is just a comment added to illustrate the use of version control.
+
 Pets={'Dogs': [{'dog': 'Chia'}, {'dog': 'Pepe'}, {'dog': 'Dodo'}], 'Cats': [{'cat': 'Kiki'}, {'cat': 'Tigra'}, {'cat': 'Gris'}, {'cat': 'Clarito'}], 'Bunnies': [{'bunny': 'Luna'}, {'bunny': 'Rico'}, {'bunny': 'Coco'}]}
